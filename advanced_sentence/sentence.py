473268d2cf1ae975b7a610aae48cbcbafd71c8e0
regesata <regesata@gmail.com>
2021-10-02 18:48:12 +0300

    Some edits in methods

--- advanced_sentence/sentence.py
+++ advanced_sentence/sentence.py
@@ -7,6 +7,7 @@ class MultipleSentenceError(Exception):
     """
     def __init__(self):
         self.message = "Multiple sentence found"
+        super().__init__()
 
     def __str__(self):
         return self.message
@@ -35,35 +36,32 @@ class Sentence:
     ----------
     sentence: str
         contains sentence for further processing
-    _space_pos: int
+
         position of firs space
-    _other_char_clothed: bool
-        flag that indicates other_chars list doesnt append symbols
-    other_chars : list
-        List of all non alphabet symbols
+
 
     """
     def __init__(self, sentence: str):
         self.sentence = sentence
         self._is_sentence()
-        self._other_char_clothed = False
+        self.curr_pos = 0
+        self.curr_word = ""
         self._words_count = 0
+        self._other_words_count = 0
         self.len = len(sentence)
-        self.other_chars = []
         self._words_counter()
-        self._space_pos = 0
-        self._new_space_pos = 0
-        self.__words_count = self._words_count
 
     def __repr__(self):
-        return f"<Sentence(words={self._words_count}, other_chars={len(self.other_chars)})>"
+        return f"<Sentence(words={self._words_count}, other_chars={self._other_words_count})>"
 
     def _is_sentence(self):
         """
-        This method checks incoming string. If it is not string  or sentence
+        This method checks incoming string.
+        If it is not string  or sentence
         raise exception:
         if sentence ends not with . ! ? or ... raise ValueError
-        if sentence consists from many sentences, has more then one occurrences of . ! ? or ... raise
+        if sentence consists from many sentences, has more then
+        one occurrences of . ! ? or ... raise
         MultipleSentencesError
 
 
@@ -95,78 +93,44 @@ class Sentence:
         if self.sentence.endswith("...") and self.sentence.count(".") > 3:
             raise MultipleSentenceError
 
-    def _trim_word(self, word: str):
-        """
-        Method trims firs and last character in word parameter
-        and returns word without it
-
-        """
-        if len(word) <= 1:
-            return word
-        if word.startswith(" "):
-            word = word[1:]
-        if not(word[-1].isalnum()) :
-            if not self._other_char_clothed:
-                self.other_chars.append(word[-1])
-            word = word[:len(word) - 1]
-        return word
-
-    def _is_word(self, word: str):
-        """
-        Method checks that word argument contains only alphabetic characters
-        return True or False and adds all non alphabetic symbols in self.other_characters attribute
-
-        """
-        flag = True
-        word = self._trim_word(word)
-        if len(word) < 1:
-            return False
-        for ch in word:
-            if not ch.isalpha() :
-                if ch not in self.other_chars and ch != " ":
-                    self.other_chars.append(ch)
-                flag = False
-        return flag
-
     def _words_counter(self):
         """
         Method counts words in sentence
         """
-        space_count = self.sentence.count(" ")
-        space_pos = 0
-        for i in range(space_count + 1):
-            new_space_pos = self.sentence.find(" ", space_pos + 1)
-            if new_space_pos > -1:
-                word = self.sentence[space_pos+1:new_space_pos]
+        curr_word = ""
+        cur_pos = 0
+        words_count = 0
+        other_words_count = 0
+        while cur_pos < len(self.sentence):
+            if self.sentence[cur_pos].isalnum():
+                curr_word += self.sentence[cur_pos]
+                cur_pos += 1
+            elif len(curr_word) > 0:
+                curr_word = ""
+                words_count += 1
             else:
-                word = self.sentence[space_pos:self.len]
-            if word.startswith(" "):
-                word = word[1:]
-            if self._is_word(word):
-                self._words_count += 1
-            space_pos = new_space_pos
-        self._other_char_clothed = True
+                cur_pos += 1
+                other_words_count += 1
+                curr_word = ""
+        self._other_words_count = other_words_count
+        self._words_count = words_count
 
     def _words(self) -> str:
         """
-        This method returns words from sentence and decrease word_count,
-        if word_count equal
-        """
+        This method returns words from sentence.
 
-        if self._words_count > 0:
-            while True:
-                self._new_space_pos = self.sentence.find(" ", self._space_pos + 1)
-                if self._new_space_pos > -1:
-                    word = self.sentence[self._space_pos:self._new_space_pos]
-                else:
-                    word = self.sentence[self._space_pos:self.len+1]
-                self._space_pos = self._new_space_pos
-                if self._is_word(word):
-                    self._words_count -= 1
-                    yield self._trim_word(word)
-        else:
-            self._words_count = self.__words_count
-            self._space_pos = 0
+        """
+        curr_word = ""
+        while self.curr_pos < self.len:
+            if self.sentence[self.curr_pos].isalnum():
+                curr_word += self.sentence[self.curr_pos]
+                self.curr_pos += 1
+            elif len(curr_word) > 0:
+                self.curr_pos += 1
+                yield curr_word
+            else:
+                self.curr_pos += 1
+        self.curr_pos = 0
 
     def __iter__(self):
         return self
@@ -175,38 +139,70 @@ class Sentence:
         return next(self._words())
 
     def __len__(self):
-        return self.__words_count
+        return self._words_count
 
     def __getitem__(self, index):
-
-        slice_res = []
+        slice_res = ""
         if isinstance(index, slice):
             start, stop, step = index.indices(len(self))
             if start > stop:
                 raise IndexError("Stop must be bigger than start")
-            for i in range(self._words_count):
-                a = next(self._words())
-                if start <= i < stop:
-                    slice_res.append(a)
-            self._words_count = self.__words_count
-            self._space_pos = 0
+            for ind in range(self._words_count):
+                word = next(self._words())
+                if start <= ind < stop:
+                    slice_res += word + " "
+            self.curr_pos = 0
             return slice_res
-        else:
-            for i in range(self._words_count):
-                a = next(self._words())
-                if i == index:
-                    self._words_count = self.__words_count
-                    return a
+
+        for ind in range(self._words_count):
+            slice_res = next(self._words())
+            if ind == index:
+                self.curr_pos = 0
+                return slice_res
 
     @property
     def words(self):
-        for _ in range(self._words_count):
-            print(self.__next__(), end=" ")
-        self._words_counter()
-        self._space_pos = 0
-        print("")
-        return
+        """
+        Property that returns all word in sentence
+        :return: str
+        """
+        curr_word = ""
+        res = []
+        curr_pos = 0
 
+        while curr_pos < self.len:
+            if self.sentence[curr_pos].isalnum():
+                curr_word += self.sentence[curr_pos]
+                curr_pos += 1
+            elif len(curr_word) > 0:
+                curr_pos += 1
+                res.append(curr_word)
+                curr_word = ""
+            else:
+                curr_pos += 1
+
+        return " ".join(res)
+
+    @property
+    def other_chars(self):
+        """
+        Property that returns all non words in sentence
+        :return: str
+        """
+
+        curr_word = ""
+        curr_pos = 0
+
+        while curr_pos < self.len:
+            if not self.sentence[curr_pos].isalnum():
+                curr_word += "'" + self.sentence[curr_pos] + "'"
+                curr_pos += 1
+            elif len(curr_word) > 0:
+                curr_pos += 1
+            else:
+                curr_pos += 1
+
+        return curr_word
 
 class SentenceIterator:
     """Iterator class for Sentence class container"""
@@ -218,29 +214,15 @@ class SentenceIterator:
         return Sentence(self.sentence)
 
 
-a = Sentence("HUIH, 33Z  zzxxx, dfd, AS zzz.")
-print(a)
+a = Sentence("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
 
+for i in a:
+    print(i)
 print(a[1:4])
-for z in a:
-    print(z)
+print(a.words)
 print(a.other_chars)
 
 
-word_iter = SentenceIterator("AA aa dsd ds.")
+word_iter = SentenceIterator("Lorem Ipsum is simply dummy text.")
 for a in word_iter:
     print(a)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
